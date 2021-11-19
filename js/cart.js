325b60d29956b96626894942f90360710258510d
RomanLienshyn <69215141+RomanLienshyn@users.noreply.github.com>
2021-11-19 11:18:26 +0200

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+function cart() {
+
+	const cartBtn = document.querySelector('.button-cart');
+	const cart = document.querySelector('#modal-cart');	
+	const closeBtn = document.querySelector('.modal-close');
+
+	cartBtn.addEventListener('click', function() {
+		cart.style.display = 'flex';
+	})
+
+	closeBtn.addEventListener('click', function() {
+		cart.style.display = '';
+	})
+	
+}
+
+cart();
---
+++ js/search.js
@@ -0,0 +1,14 @@
+function search() {
+
+ 	const input = document.querySelector('.search-block > input');
+ 	const searchBtn = document.querySelector('.search-block > button');	
+
+	input.addEventListener('input', () => {
+ 		searchBtn.addEventListener('click', () => {
+ 			console.log(input.value);
+ 		});
+ 	});
+ 	
+}
+
+search();
