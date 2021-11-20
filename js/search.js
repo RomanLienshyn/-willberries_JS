92661e362bca79cf144900a48ef8d178733b64a1
RomanLienshyn <69215141+RomanLienshyn@users.noreply.github.com>
2021-11-20 08:30:47 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -3,11 +3,16 @@ function search() {
  	const input = document.querySelector('.search-block > input');
  	const searchBtn = document.querySelector('.search-block > button');	
 
-	input.addEventListener('input', () => {
- 		searchBtn.addEventListener('click', () => {
- 			console.log(input.value);
- 		});
- 	});
+// 	input.addEventListener('input', () => {
+//  		searchBtn.addEventListener('click', () => {
+//  			console.log(input.value);
+//  		});
+//  	});
+	
+	searchBtn.addEventListener('click', () => {
+		console.log(input.value);
+	});	
+	
  	
 }
 
